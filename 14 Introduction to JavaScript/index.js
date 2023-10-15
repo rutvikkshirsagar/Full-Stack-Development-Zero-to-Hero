62492c095b034b989d79e38cb7674530b1a3c1fa
Rutvik Kshirsagar <64627049+rutvikkshirsagar@users.noreply.github.com>
2023-10-15 14:56:06 +0530

    Introduction to JavaScriptl

---
+++ 14 Introduction to JavaScript/index.js
@@ -0,0 +1,61 @@
+// // Declare variable
+// var name = "Rutvik";
+// // Print of Console
+// console.log("Jay Swami Samarth Maharaj!!");
+// //Give alert message
+// alert("OM GAN GANAPATEY NAMAH!!");
+// //Calculate size of variable
+// console.log(name.length);
+// //Take a prompt input
+// var myName = prompt("What's Your Name?");
+// //String Slicing
+// console.log(name.slice(0,3));
+// //String to upper & toLowerCase
+// console.log(name.toUpperCase());
+// console.log(name.toLowerCase());
+
+
+// var eassay = prompt("Enter a twitter eassay!!");
+
+// var size = eassay.length;
+// var rem = 140 - size;
+
+// alert("You have written "+ size +" characters, "+"you have "+ rem+" characters left.");
+
+/*// Slice the passage at 140 characters
+var eassay = prompt("Enter a twitter eassay!!");
+alert(eassay.slice(0,140));*/
+
+//String Casing Solution
+// var name = prompt("What's your good name?");
+// alert("Hello "+name.slice(0,1).toUpperCase()+name.slice(1,name.length).toLowerCase());
+
+//Calculate age of a dog if he is human
+// var dogAge = prompt("What's your dog age?");
+
+// alert(((dogAge-2)*4)+21);
+
+// var x = 3;
+// var y = x++;
+// console.log(y);
+
+// var output = [];
+// var temp = 1;
+// function fizzBuzz(){
+//     if(temp%3===0 && temp%5===0){
+//         output.push("fizzBuzz");
+//     }
+//     else if(temp%3===0){
+//         output.push("fizz");
+//     }
+//     else if(temp%5===0){
+//         output.push("Buzz");
+//     }
+//     else{
+//         output.push(temp);
+//     }
+    
+//     temp++;
+//     console.log(output);
+// }
+
